0155f8dfd94ca8e611b7a88c20b9ac0281fbfdd6
Kelly Mathews <kelly@picwell.com>
2016-12-12 14:39:19 -0500

    setup package for pypi

--- setup.py
+++ setup.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 requirements = [
     'schematics<=1.0.2',
     'six',
 ]
 
-setup(name='schematics-extensions',
-      version='0.0.5',
+setup(name='schematics_extensions',
+      packages=find_packages(exclude=['*.test', '*.tests']),
+      version='0.1',
       description='Extensions for the Schematics library',
       author='Picwell',
       author_email='dev@picwell.com',
       url='http://github.com/picwell/schematics-extensions',
-      packages=['schematics_extensions'],
-      install_requires=requirements)
+      install_requires=requirements,
+      dependency_links=[],
+      include_package_data=True)
