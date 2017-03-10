fb0c78f2fb0a9c775ca3532252e2d584eb04ef1e
Trevor <trevor@picwell.com>
2017-03-10 16:08:48 -0500

    Bump version and use more pleasing args line breaking

--- setup.py
+++ setup.py
@@ -6,11 +6,13 @@ requirements = [
     'schematics==1.1.1',
 ]
 
-setup(name='schematics-extensions',
-      version='0.0.5',
-      description='Extensions for the Schematics library',
-      author='Picwell',
-      author_email='dev@picwell.com',
-      url='http://github.com/picwell/schematics-extensions',
-      packages=find_packages(),
-      install_requires=requirements)
+setup(
+    name='schematics-extensions',
+    version='0.0.6',
+    description='Extensions for the Schematics library',
+    author='Picwell',
+    author_email='dev@picwell.com',
+    url='http://github.com/picwell/schematics-extensions',
+    packages=find_packages(),
+    install_requires=requirements
+)
